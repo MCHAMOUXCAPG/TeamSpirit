8d63599cec20ce741afad0cc6e427ab657c3ac0d
Iwan van der Kleijn <ivanderk@capgemini.com>
2020-05-19 16:23:56 +0200

    Router example

--- api/main.go
+++ api/main.go
@@ -9,7 +9,20 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Root site")
 	})
+	e.GET("/users/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users/:id")
+	})
+
+	e.GET("/users/new", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users/new")
+	})
+
+	e.GET("/users/1/files/*", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users/1/files/*")
+	})
+
 	e.Logger.Fatal(e.Start(":1234"))
+
 }

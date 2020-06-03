c96cc2a9f9f7d94b30489cc17e34e3711c01ce4d
Juan Carlos Gimeno Agustin <juan-carlos.gimeno-agustin@capgemini.com>
2020-06-03 08:49:25 +0200

    Updated colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import colors from "./src/config/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ color: colors.primary }}>
+        Welcome to Team Spirit Survey!
+      </Text>
     </View>
   );
 }
@@ -12,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,8 +1,9 @@
 export default {
   primary: "#79C0C6",
-  secondary: "#C4C4C4",
+  secondary: "rgba(121, 192, 198, 0.5)",
+  accent: "#C4C4C4",
   green: "#9BC183",
-  red: "FE5454",
+  red: "#FE5454",
   white: "#fff",
   black: "#000",
 };

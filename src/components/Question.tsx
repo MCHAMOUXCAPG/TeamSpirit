80c47ef8fd3cb4789467852458077c809ccc926e
Bryson Leon <brleon@capgemini.com>
2020-06-05 11:29:48 +0200

    Created Question Component

---
+++ src/components/Question.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import colors from "../config/colors";
+
+const Question = (props) => {
+  return (
+    <View style={Styles.container}>
+      <Text style={Styles.number}> Question {props.questionNum} </Text>
+      <Text style={Styles.question}> {props.text} </Text>
+      <View style={Styles.emojis}>{props.emojis}</View>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: colors.primary,
+    borderRadius: 20,
+    borderWidth: 1,
+    backgroundColor: colors.white,
+    margin: 15,
+    height: 360,
+    width: "90%",
+    elevation: 3,
+    paddingTop: 50,
+    paddingBottom: 50,
+    paddingHorizontal: 10,
+  },
+  number: {
+    fontWeight: "100",
+    fontSize: 18,
+  },
+  question: {
+    fontSize: 24,
+    textAlign: "center",
+  },
+  emojis: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+});
+
+export default Question;
--- src/screens/SurveyScreen.tsx
+++ src/screens/SurveyScreen.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
+import Question from "../components/Question";
+
 const SurveyScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Survey!</Text>
+      <Text style={styles.text}> SurveyScreen! </Text>
+      <Question />
     </View>
   );
 };

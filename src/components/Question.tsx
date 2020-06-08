03dc3742a0e1c59ca664c8c8c654ba83d3b04a7f
Bryson Leon <brleon@capgemini.com>
2020-06-08 17:18:45 +0200

    Added slider and star modules.

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, Slider } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Slider from "react-native-slider";
+import { AirbnbRating } from "react-native-ratings";
 
 import { questionType } from "../models/interfaces";
 import colors from "../config/colors";
+
+const ratingCompleted = (starRating) => {
+  console.log(starRating);
+}
+
+const slidingCompleted = (slideRating) => {
+  console.log(slideRating);
+}
 
 const Question = ({
   number,
@@ -22,18 +32,61 @@ const Question = ({
       {type == questionType.fiveIcons ? (
         <View style={Styles.emojis}>
           {images.map((image, index) => {
-            return <Image key={index} source={image} height={20} width={20} />;
+            return <TouchableOpacity><Image key={index} source={image} style={{ height: 40, width: 40 }} /></TouchableOpacity>;
           })}
         </View>
       ) : null}
-      {type == questionType.slider ? (
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
+       {type == questionType.slider ? (
+          <View style={Styles.emojis}>
+           <TouchableOpacity>
+              <Image
+                style={{ height: 40, width: 40 }}
+                source={images[0]}
+              />
+            </TouchableOpacity>
+             <Slider 
+          value={2} 
+          minimumValue={0} 
+          maximumValue={4} 
+          step={1} 
+          minimumTrackTintColor={colors.primary} 
+          thumbTintColor={colors.primary} 
+          onSlidingComplete={slidingCompleted} 
+          style={{width: "50%"}}  
         />
+            <TouchableOpacity>
+              <Image
+                style={{ height: 40, width: 40 }}
+                source={images[1]}
+              />
+            </TouchableOpacity>
+        </View>
+       
+      ) : null}
+      {type == questionType.stars ? (
+        <AirbnbRating 
+          defaultRating={0} 
+          count={5} 
+          size={40} 
+          showRating={false} 
+          onFinishRating={ratingCompleted} 
+        />
+      ) : null}
+      {type == questionType.twoIcons ? (
+        <View style={Styles.emojis}>
+           <TouchableOpacity>
+              <Image
+                style={{ height: 40, width: 40 }}
+                source={images[0]}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={{ height: 40, width: 30 }}
+                source={images[1]}
+              />
+            </TouchableOpacity>
+        </View>
       ) : null}
     </View>
   );
@@ -68,6 +121,9 @@ const Styles = StyleSheet.create({
     justifyContent: "space-around",
     width: "100%",
   },
+  rating: {
+    
+  }
 });
 
 export default Question;

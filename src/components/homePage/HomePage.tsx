ffb3e8f4ef0ae3b8425f9b31a20c911ab51146f6
Avellan Poma-external <eduardo-alfredo.avellan-poma-external@capgemini.com>
2020-06-09 13:54:57 +0200

    Home Page Layout

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./HomePage.css";
+import Background from "../../assets/homePageBackground.png";
+import TextField from "@material-ui/core/TextField";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 
 function HomePage() {
-  return <div>Home Page Component</div>;
+  var sectionStyle = {
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: `url(${Background})`,
+  };
+  return (
+    <div id="bgImg" style={sectionStyle}>
+      <div className="headerSurvey">Welcome to <br/> Team Spirit Survey</div>
+      <form noValidate autoComplete="off">
+        <Card id="Card">
+          <CardContent>
+            <TextField
+              required
+              id="standard-required"
+              variant="outlined"
+              placeholder="Enter your code in captial letters..."
+            />
+          </CardContent>
+          <CardActions>
+            <Button id ="ButtonStart"onClick={() => { alert('clicked') }} size="small">Start</Button>
+          </CardActions>
+        </Card>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;

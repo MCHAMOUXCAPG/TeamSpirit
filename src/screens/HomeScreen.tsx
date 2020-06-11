bd402f7cdaf6adee570f4e03eb2556dc01d1157d
Juan Carlos Gimeno Agustin <juan-carlos.gimeno-agustin@capgemini.com>
2020-06-11 12:50:28 +0200

    Added AvoidingKeyboardView

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
   Keyboard,
   ImageBackground,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
 } from "react-native";
 import colors from "../config/colors";
 
@@ -37,58 +39,58 @@ const HomeScreen = ({ navigation }: { navigation: any }) => {
   };
 
   return (
-    <View style={styles.screen}>
-      <ImageBackground
-        source={require("../assets/homeBackground.png")}
-        style={styles.imgBackground}
-      >
-        <View style={styles.contentAbove}>
-          <Text style={styles.title}>Welcome to Team Spirit Survey!</Text>
-        </View>
-        <View style={styles.contentBelow}>
-          <Text style={styles.textSentence1}>
-            Please, answer the following 6 questions. It will take you a couple
-            of minutes.
-          </Text>
-          <Text style={styles.textSentence2}>
-            But first, write the code of your team:
-          </Text>
-          <TextInput
-            style={styles.input}
-            placeholder="My project is..."
-            onChangeText={(inputText) => setInputText(inputText)}
-            value={inputText}
-            maxLength={10}
-          />
+    <KeyboardAvoidingView style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <ImageBackground
+            source={require("../assets/homeBackground.png")}
+            style={styles.imgBackground}
+          >
+            <View style={styles.contentAbove}>
+              <Text style={styles.title}>Welcome to Team Spirit Survey!</Text>
+            </View>
+            <View style={styles.contentBelow}>
+              <Text style={styles.textSentence1}>
+                Please, answer the following 6 questions. It will take you a
+                couple of minutes.
+              </Text>
+              <Text style={styles.textSentence2}>
+                But first, write the code of your team:
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="My project is..."
+                onChangeText={(inputText) => setInputText(inputText)}
+                value={inputText}
+                maxLength={10}
+              />
 
-          <TouchableOpacity onPress={submitHandler} style={styles.submit}>
-            <Text style={styles.insideButton}>Start</Text>
-          </TouchableOpacity>
+              <TouchableOpacity onPress={submitHandler} style={styles.submit}>
+                <Text style={styles.insideButton}>Start</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
         </View>
-      </ImageBackground>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    left: 0,
-    top: 0,
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
   },
   contentAbove: {
     flex: 1,
-    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
   contentBelow: {
     flex: 1,
-    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: 50,
   },
   title: {
     position: "absolute",
@@ -103,9 +105,7 @@ const styles = StyleSheet.create({
     color: colors.primary,
   },
   textSentence1: {
-    position: "absolute",
-    width: "70%",
-    top: "1%",
+    width: "90%",
     fontFamily: "Roboto",
     fontStyle: "normal",
     fontWeight: "normal",
@@ -113,11 +113,10 @@ const styles = StyleSheet.create({
     lineHeight: 16,
     textAlign: "center",
     color: colors.white,
+    marginBottom: 30,
   },
   textSentence2: {
-    position: "absolute",
-    width: "70%",
-    top: "18%",
+    width: "90%",
     fontFamily: "Roboto",
     fontStyle: "normal",
     fontWeight: "normal",
@@ -127,34 +126,30 @@ const styles = StyleSheet.create({
     color: colors.white,
   },
   input: {
-    position: "absolute",
     width: "45%",
-    height: "8%",
-    top: "26%",
+    height: 30,
     backgroundColor: colors.white,
     borderRadius: 20,
+    marginBottom: 30,
+    marginTop: 30,
+    paddingLeft: 10,
   },
   submit: {
-    position: "absolute",
     width: "45%",
-    top: "47%",
+    height: 70,
     borderColor: colors.white,
     borderWidth: 1,
     borderRadius: 50,
-    paddingLeft: "20%",
-    paddingRight: "20%",
-    paddingTop: "10%",
-    paddingBottom: "10%",
   },
   imgBackground: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
     flex: 1,
+    resizeMode: "contain",
+    justifyContent: "center",
   },
   insideButton: {
     textAlign: "center",
     fontSize: 24,
+    lineHeight: 65,
     color: colors.white,
   },
 });

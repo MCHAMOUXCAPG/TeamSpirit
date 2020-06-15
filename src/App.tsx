2f5c1eb0db841a8e31bda31da9261553075cb1a3
Juan Carlos Gimeno Agustin <juan-carlos.gimeno-agustin@capgemini.com>
2020-06-15 09:10:14 +0200

    Added survey auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import HomePage from "./components/homePage/HomePage";
 import Survey from "./components/survey/Survey";
 import ExitPage from "./components/exitPage/ExitPage";
+import PrivateRoute from "./auth/PrivateRoute";
+import { AuthContext } from "./context/auth";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/survey" element={<Survey />} />
-        <Route path="/success" element={<ExitPage />} />
-      </Routes>
-    </Router>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/success" element={<ExitPage />} />
+          <PrivateRoute
+            path="/survey"
+            element={<Survey />}
+            alternativePath="/"
+          />
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 

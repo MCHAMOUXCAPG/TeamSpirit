73b04718908d7a3ad40684a85928432a1511697f
Avellan Poma-external <eduardo-alfredo.avellan-poma-external@capgemini.com>
2020-06-19 11:09:54 +0200

    Team-Status-add

--- src/components/teamHomePage/TeamHomePage.tsx
+++ src/components/teamHomePage/TeamHomePage.tsx
@@ -3,9 +3,8 @@ import "./TeamHomePage.css";
 import { Container, Grid } from "@material-ui/core";
 import NavBar from "../navBar/NavBar";
 import AverageChart from "../averageChart/Chart";
+import SurveyStatus from "../surveyStatus/SurveyStatus";
 const TeamHomePage = () => {
-  const [sliderStep, setSliderStep] = useState<number | number[]>(5);
-
   return (
     <div>
       <NavBar user={true}></NavBar>
@@ -24,7 +23,12 @@ const TeamHomePage = () => {
             <AverageChart grade={20} />
           </Grid>
           <Grid item xs={6} md={6}>
-            <div className="Texto">Team Spirit Survey</div>
+            <SurveyStatus
+              period={"25-jun 2020 to 26 jun 2020"}
+              completed={"4/5"}
+              currentResult={7.9}
+              historicResult={7.6}
+            />
           </Grid>
           <Grid item xs={12}>
             <div className="Texto">Team Spirit Survey</div>

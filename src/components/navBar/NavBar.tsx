874bcbfbb7a5bbaa5e42160fad3361a886a7423e
Juan Carlos Gimeno Agustin <juan-carlos.gimeno-agustin@capgemini.com>
2020-06-29 13:20:04 +0200

    HandleClick exit Button

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import Group from "@material-ui/icons/Group";
+import { useNavigate } from "react-router-dom";
 
 import "./NavBar.css";
 import logo from "../../assets/logo.png";
+import { AuthContext } from "../../context/auth";
 
 function NavBar({ user }: { user: boolean }) {
+  const context = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    context.setValid(false);
+    navigate("/");
+  };
+
   return (
     <AppBar position="static" className="navBar">
       <Toolbar>
         <Grid container direction="row" justify="flex-end" alignItems="center">
           {user ? (
             <Grid item xs={5} sm={9} className="navBarButtons">
-              <Button color="inherit" startIcon={<ExitToApp />}>
+              <Button
+                color="inherit"
+                startIcon={<ExitToApp />}
+                onClick={() => {
+                  handleClick();
+                }}
+              >
                 Sign Out
               </Button>
               <Button color="inherit" startIcon={<Group />}>

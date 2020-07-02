a0dbad243f9b2288dd4405840fbd943aae3f052a
Anas Gharbi <gharbi.anas@capgemini.com>
2020-07-02 12:03:22 +0200

    is allowed to vote

--- api/services/survey-service.go
+++ api/services/survey-service.go
@@ -127,11 +127,29 @@ func AddNotesToSurvey(c echo.Context) error {
 
 	survey, _ := SurveyRepo.GetSurvey(surveyCode)
 
+	if isNotAllowedToVote(survey.Notes, notes) {
+		return c.JSON(http.StatusNotAcceptable, echo.Map{
+			"Status":   http.StatusNotAcceptable,
+			"Message":  "You cannot send your feedback two times",
+			"path":     "api/services/survey-service.go",
+			"function": "AddNotesToSurvey",
+		})
+	}
+
 	survey.Notes = notes
 
 	SurveyRepo.UpdateSurvey(surveyCode, survey)
 
 	return c.JSON(http.StatusOK, survey)
+}
+
+func isNotAllowedToVote(surveyNotes []entities.Note, bodyNotes []entities.Note) bool {
+	for _, note := range surveyNotes {
+		if note.User == bodyNotes[0].User {
+			return true
+		}
+	}
+	return false
 }
 
 func hashAndSaltUser(notes []entities.Note) []entities.Note {
@@ -141,7 +159,6 @@ func hashAndSaltUser(notes []entities.Note) []entities.Note {
 		hash := sha256.Sum256([]byte(note.User))
 		hashSlice := hash[:]
 		hashToString := hex.EncodeToString(hashSlice)
-
 		note.User = hashToString
 		result = append(result, note)
 	}

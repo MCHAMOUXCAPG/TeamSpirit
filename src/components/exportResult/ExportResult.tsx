cd269990e6cbc57c54fe7f148199711f1b85aa6f
vvigertk <66320854+vvigertk@users.noreply.github.com>
2020-07-03 12:56:40 +0200

    Added necessary components and styles, missing functionality

--- src/components/exportResult/ExportResult.tsx
+++ src/components/exportResult/ExportResult.tsx
@@ -6,20 +6,50 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
+import { Container, Grid } from "@material-ui/core";
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import {
+  createStyles,
+  makeStyles,
+  withStyles,
+  Theme,
+} from "@material-ui/core/styles";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import InputBase from "@material-ui/core/InputBase";
+import Button from "@material-ui/core/Button";
+import GetAppIcon from "@material-ui/icons/GetApp";
+
+const BootstrapInput = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "label + &": {
+        marginTop: theme.spacing(10),
+      },
+    },
+    input: {
+      borderRadius: 28,
+      position: "relative",
+      width: 150,
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #919191",
+      fontSize: 16,
+      padding: "10px 26px 10px 12px",
+      // Use the system font instead of the default Roboto font.
+      fontFamily: ["Roboto"].join(","),
+      "&:focus": {
+        borderRadius: 20,
+        borderColor: "#79C0C6",
+        boxShadow: "#79C0C6",
+      },
+    },
+  })
+)(InputBase);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    formControl: {
+    margin: {
       margin: theme.spacing(1),
-      minWidth: 150,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
+      top: -18,
     },
   })
 );
@@ -42,26 +72,66 @@ function ExportResult() {
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            <p>
-              From:
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel className="form-control">Sprint1</InputLabel>
-                <Select
-                  labelId="demo-simple-select-outlined-label"
-                  id="prueba"
-                  //value={}
-                  //onChange={}
-                  label="Sprint"
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
+            <p className="info-text">
+              Please, select the Sprints and export your CSV.
             </p>
+            <div>
+              <Container
+                maxWidth="lg"
+                className="grid-content"
+                disableGutters={true}
+              >
+                <Grid container spacing={8} direction="row" justify="center">
+                  <Grid item xs={1}>
+                    <p className="text">From:</p>
+                  </Grid>
+                  <Grid item xs={3} className="text">
+                    <FormControl className={classes.margin}>
+                      <NativeSelect
+                        id="demo-customized-select-native"
+                        //value={age}
+                        //onChange={handleChange}
+                        input={<BootstrapInput />}
+                      >
+                        <option aria-label="None" value="" />
+                        <option value={10}>Sprint1</option>
+                        <option value={20}>Sprint2</option>
+                        <option value={30}>Sprint3</option>
+                      </NativeSelect>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <p className="text" id="p-position">
+                      To:
+                    </p>
+                  </Grid>
+                  <Grid item xs={3} className="text">
+                    <FormControl className={classes.margin}>
+                      <NativeSelect
+                        id="demo-customized-select-native"
+                        //value={age}
+                        //onChange={handleChange}
+                        input={<BootstrapInput />}
+                      >
+                        <option aria-label="None" value="" />
+                        <option value={10}>Sprint1</option>
+                        <option value={20}>Sprint2</option>
+                        <option value={30}>Sprint3</option>
+                      </NativeSelect>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Button
+                      variant="outlined"
+                      className="bt btn-containe"
+                      startIcon={<GetAppIcon />}
+                    >
+                      Export
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Container>
+            </div>
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>

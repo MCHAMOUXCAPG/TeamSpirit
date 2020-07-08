7a016a7d3a08e74b1131dce392de9477d654079f
vvigertk <66320854+vvigertk@users.noreply.github.com>
2020-07-08 14:56:35 +0200

    changed to correct component for DatePicker

--- src/components/exportResult/ExportResult.tsx
+++ src/components/exportResult/ExportResult.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExportResult.css";
 import NoteAddIcon from "@material-ui/icons/NoteAdd";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
@@ -7,54 +7,18 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Container, Grid } from "@material-ui/core";
-import FormControl from "@material-ui/core/FormControl";
-import {
-  createStyles,
-  makeStyles,
-  withStyles,
-  Theme,
-} from "@material-ui/core/styles";
-import NativeSelect from "@material-ui/core/NativeSelect";
-import InputBase from "@material-ui/core/InputBase";
 import Button from "@material-ui/core/Button";
 import GetAppIcon from "@material-ui/icons/GetApp";
-
-const BootstrapInput = withStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      "label + &": {
-        marginTop: theme.spacing(10),
-      },
-    },
-    input: {
-      borderRadius: 28,
-      position: "relative",
-      width: 150,
-      backgroundColor: theme.palette.background.paper,
-      border: "2px solid #919191",
-      fontSize: 16,
-      padding: "10px 26px 10px 12px",
-      // Use the system font instead of the default Roboto font.
-      fontFamily: ["Roboto"].join(","),
-      "&:focus": {
-        borderRadius: 20,
-        borderColor: "#79C0C6",
-        boxShadow: "#79C0C6",
-      },
-    },
-  })
-)(InputBase);
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    margin: {
-      margin: theme.spacing(1),
-      top: -18,
-    },
-  })
-);
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 function ExportResult() {
+  const [startDate, setStartDate] = React.useState<Date | null>(new Date());
+  const [endDate, setEndDate] = React.useState<Date | null>(new Date());
   const classes = useStyles();
 
   return (
@@ -85,40 +49,52 @@ function ExportResult() {
                   <Grid item xs={1}>
                     <p className="text">From:</p>
                   </Grid>
-                  <Grid item xs={3} className="text">
-                    <FormControl className={classes.margin}>
-                      <NativeSelect
-                        id="demo-customized-select-native"
-                        //value={age}
-                        //onChange={handleChange}
-                        input={<BootstrapInput />}
-                      >
-                        <option aria-label="None" value="" />
-                        <option value={10}>Sprint1</option>
-                        <option value={20}>Sprint2</option>
-                        <option value={30}>Sprint3</option>
-                      </NativeSelect>
-                    </FormControl>
+                  <Grid item xs={3}>
+                    <div className="date-position">
+                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                          autoOk
+                          disableToolbar
+                          variant="inline"
+                          format="MM/dd/yyyy"
+                          inputVariant="outlined"
+                          margin="normal"
+                          className={classes.root}
+                          id="date-picker-inline"
+                          value={startDate}
+                          onChange={(date: any) => setStartDate(date)}
+                          KeyboardButtonProps={{
+                            "aria-label": "change date",
+                          }}
+                        />
+                      </MuiPickersUtilsProvider>
+                    </div>
                   </Grid>
                   <Grid item xs={1}>
                     <p className="text" id="p-position">
                       To:
                     </p>
                   </Grid>
-                  <Grid item xs={3} className="text">
-                    <FormControl className={classes.margin}>
-                      <NativeSelect
-                        id="demo-customized-select-native"
-                        //value={age}
-                        //onChange={handleChange}
-                        input={<BootstrapInput />}
-                      >
-                        <option aria-label="None" value="" />
-                        <option value={10}>Sprint1</option>
-                        <option value={20}>Sprint2</option>
-                        <option value={30}>Sprint3</option>
-                      </NativeSelect>
-                    </FormControl>
+                  <Grid item xs={3}>
+                    <div className="date-position">
+                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker
+                          autoOk
+                          disableToolbar
+                          variant="inline"
+                          format="MM/dd/yyyy"
+                          inputVariant="outlined"
+                          margin="normal"
+                          className={classes.root}
+                          id="date-picker-inline"
+                          value={endDate}
+                          onChange={(date: any) => setEndDate(date)}
+                          KeyboardButtonProps={{
+                            "aria-label": "change date",
+                          }}
+                        />
+                      </MuiPickersUtilsProvider>
+                    </div>
                   </Grid>
                   <Grid item xs={4}>
                     <Button
@@ -138,5 +114,23 @@ function ExportResult() {
     </div>
   );
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+          borderColor: "#919191",
+        },
+        "&:hover fieldset": {
+          borderColor: "#919191",
+        },
+        "&.Mui-focused fieldset": {
+          borderColor: "#919191",
+        },
+      },
+    },
+  })
+);
 
 export default ExportResult;

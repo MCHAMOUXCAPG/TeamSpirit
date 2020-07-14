d210b162cf3bfbb9e3019feb97b61755d017fa60
Avellan Poma <eduardo-alfredo.avellan-poma-external@capgemini.com>
2020-07-14 13:18:01 +0200

    add-tab

--- src/components/administratorPage/AdministratorPage.tsx
+++ src/components/administratorPage/AdministratorPage.tsx
@@ -3,32 +3,26 @@ import "./AdministratorPage.css";
 import { Button, Container, Grid } from "@material-ui/core";
 import { ManageUserService } from "../../services/Services";
 import NavBar from "../navBar/NavBar";
-import AppBar from "@material-ui/core/AppBar";
+
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 
 const AdministratorPage = () => {
-  const usersList: ManageUserService = new ManageUserService();
-  const token = sessionStorage.getItem("token");
-
   const [value, setValue] = React.useState(0);
-  interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-  }
-  function a11yProps(index: any) {
-    return {
-      id: `simple-tab-${index}`,
-      "aria-controls": `simple-tabpanel-${index}`,
-    };
-  }
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
+
+  function a11yProps(index: any) {
+    return {
+      id: `full-width-tab-${index}`,
+      "aria-controls": `full-width-tabpanel-${index}`,
+    };
+  }
+  const usersList: ManageUserService = new ManageUserService();
+  const token = sessionStorage.getItem("token");
+
   async function getAllUsers(token: string | null) {
     await usersList
       .getUsers(token)
@@ -38,25 +32,6 @@ const AdministratorPage = () => {
       .catch((err) => {
         console.log(err);
       });
-  }
-  function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
   }
 
   getAllUsers(token);
@@ -78,26 +53,20 @@ const AdministratorPage = () => {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <AppBar position="static">
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="simple tabs example"
-              >
-                <Tab label="Item One" {...a11yProps(0)} />
-                <Tab label="Item Two" {...a11yProps(1)} />
-                <Tab label="Item Three" {...a11yProps(2)} />
-              </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-              Item One
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              Item Three
-            </TabPanel>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="simple tabs example"
+            >
+              <Tab label="Users" {...a11yProps(0)} id="user-tab" />
+              <Tab label="Teams" {...a11yProps(1)} id="question-tab" />
+              <Tab label="Surveys" {...a11yProps(2)} id="question-tab" />
+            </Tabs>
+          </Grid>
+          <Grid item xs={12} id="Grid-adm-inner">
+            {value === 0 && <div>Item 1</div>}
+            {value === 1 && <div>Item 2</div>}
+            {value === 2 && <div>Item 3</div>}
           </Grid>
         </Grid>
       </Container>

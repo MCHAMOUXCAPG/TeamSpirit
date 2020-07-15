8a29ec07d30d72a047deabaf20d7253dd8d0c7d5
vvigertk <66320854+vvigertk@users.noreply.github.com>
2020-07-15 13:45:55 +0200

    solved problems, added test, changed test structure

---
+++ cypress/integration/fillSurveyTest.spec.js
@@ -0,0 +1,75 @@
+describe("Fill Survey Test", () => {
+  it("Enter with invalid code", () => {
+    cy.visit("/");
+    cy.get("#outlined-required").type("PRUEBA-mkdnr");
+    cy.get("#ButtonStart").click();
+    cy.get(".MuiFormHelperText-root.Mui-error").should(($error) => {
+      expect($error).to.contain(
+        "Some error happened when calling the method SurveyRepo.GetSurvey"
+      );
+    });
+  });
+
+  it("Enter with out of date code", () => {
+    cy.visit("/");
+    cy.get("#outlined-required").type("FINEOS-abcd");
+    cy.get("#ButtonStart").click();
+    cy.get(".MuiFormHelperText-root.Mui-error").should(($error) => {
+      expect($error).to.contain(
+        "The deadline to complete the survey has passed"
+      );
+    });
+  });
+
+  it("Enter with all participants survey filled code", () => {
+    cy.visit("/");
+    cy.get("#outlined-required").type("SNCF-klmnp");
+    cy.get("#ButtonStart").click();
+    cy.get(".MuiFormHelperText-root.Mui-error").should(($error) => {
+      expect($error).to.contain("The maximum number of notes has been reached");
+    });
+  });
+
+  it("Enter with valid code", () => {
+    cy.visit("/");
+    cy.get("#outlined-required").type("PRUEBA-ABCDE");
+    cy.get("#ButtonStart").click();
+    cy.url().should("include", "/survey");
+    cy.get("#pop-up-dialog").should(($dialog) => {
+      expect($dialog).to.contain("Please answer the 6 following questions.");
+    });
+    cy.get("#alert-dialog-button").click();
+
+    cy.get("#root > div > div > div").should(($items) => {
+      expect($items).to.have.length(8);
+      expect($items.eq(0)).to.contain("PRUEBA");
+      expect($items.eq(1)).to.contain("1");
+      expect($items.eq(2)).to.contain("2");
+      expect($items.eq(3)).to.contain("3");
+      expect($items.eq(4)).to.contain("4");
+      expect($items.eq(5)).to.contain("5");
+      expect($items.eq(6)).to.contain("6");
+      expect($items.eq(7)).to.contain("SUBMIT");
+    });
+    cy.get("#submit-btn").should("be.disabled");
+    cy.get("#root img").should(($images) => {
+      expect($images).to.have.length(17);
+      $images.eq(5).click();
+      $images.eq(10).click();
+      $images.eq(12).click();
+      $images.eq(14).click();
+      $images.eq(16).click();
+    });
+    cy.get("label[for=simple-5]").click();
+    cy.get("#submit-btn").should("not.be.disabled");
+    cy.get("#submit-btn").click();
+    cy.get("#pop-up-dialog").should(($dialog) => {
+      expect($dialog).to.contain("Form successfully submitted.");
+    });
+    cy.get("#alert-dialog-button").click();
+    cy.url().should("include", "/success");
+    cy.get(".sentence").should(($message) => {
+      expect($message).to.contain("Your opinion counts!");
+    });
+  });
+});
